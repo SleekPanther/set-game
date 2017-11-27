94f36a1bbded403b99516edb26d50757f23ba5ea
SleekPanther <macrobyte2@gmail.com>
2017-11-27 17:54:55 -0500

    check valid sets, extract combinations to separate method

--- SetGame.java
+++ SetGame.java
@@ -2,6 +2,7 @@ import java.util.*;
 
 public class SetGame {
 	private ArrayList<Card> cards = new ArrayList<Card>();
+	private ArrayList<Card[]> validSets = new ArrayList<Card[]>();
 
 	public void setCards(Card[] inputCards){
 		for(int i=0; i<inputCards.length; i++){
@@ -9,7 +10,59 @@ public class SetGame {
 		}
 	}
 
+	public ArrayList<Card> getCards(){
+		return cards;
+	}
+
 	public void findSets(){
+		//Find all possible subsets of size 3 (n choose 3)
+		for(int i=0; i<cards.size(); i++){
+			for(int j=i+1; j<cards.size(); j++){
+				for(int k=j+1; k<cards.size(); k++){
+					Card[] tempSet = {cards.get(i), cards.get(j), cards.get(k)};
+					if(isSetValid(cards.get(i), cards.get(j), cards.get(k))){
+						validSets.add(tempSet);
+					}
+				}
+			}
+		}
+
+		if(validSets.isEmpty()){
+			System.out.println("No Valid Sets. Must Draw more cards.");
+		}
+		else{
+			System.out.println("Valid Sets");
+			for(Card[] set : validSets){
+				for(Card card : set){
+					System.out.print(card);
+				}
+				System.out.println();
+			}
+		}
+	}
+
+	private boolean isSetValid(Card card1, Card card2, Card card3){
+		if(!areAllEqual(card1.number, card2.number, card3.number) && !areAllDifferent(card1.number, card2.number, card3.number)){
+			return false;
+		}
+		if(!areAllEqual(card1.shape, card2.shape, card3.shape) && !areAllDifferent(card1.shape, card2.shape, card3.shape)){
+			return false;
+		}
+		if(!areAllEqual(card1.color, card2.color, card3.color) && !areAllDifferent(card1.color, card2.color, card3.color)){
+			return false;
+		}
+		return true;
+	}
+
+	private boolean areAllEqual(int num1, int num2, int num3){
+		return num1==num2 && num2==num3;
+	}
+
+	private boolean areAllDifferent(int num1, int num2, int num3){
+		return num1 != num2 && num2 != num3 && num1 != num3;
+	}
+
+	public void printCombinations(ArrayList<Card> cards){
 		int combinationCount=0;
 		for(int i=0; i<cards.size(); i++){
 			for(int j=i+1; j<cards.size(); j++){
@@ -26,20 +79,23 @@ public class SetGame {
 	public static void main(String[] args) {
 		SetGame game = new SetGame();
 		game.setCards(new Card[] {
-				// new Card(1, 2, 3),
-				// new Card(3, 2, 1),
-				// new Card(1, 1, 2),
-				// new Card(1, 1, 1)
-				new Card(1, 1, 1), 
-				new Card(2, 2, 2), 
-				new Card(3, 3, 3), 
-				new Card(4, 4, 4), 
-				new Card(5, 5, 5)
+				new Card(1, 2, 3),
+				new Card(2, 1, 2),
+				new Card(3, 3, 1),
+				new Card(1, 1, 1)
+				// new Card(1, 1, 1), 
+				// new Card(2, 2, 2), 
+				// new Card(3, 3, 3), 
+				// new Card(4, 4, 4), 
+				// new Card(5, 5, 5)
 			}
 		);
-		
-		game.findSets();
 
+		System.out.println("All Combinations");
+		game.printCombinations(game.getCards());
+		
+		System.out.println();
+		game.findSets();
 	}
 }
 
@@ -52,6 +108,10 @@ class Card{
 		this.number=number;
 		this.shape=shape;
 		this.color=color;
+	}
+
+	public int[] getCardAsArray(){
+		return new int[]{number, shape, color};
 	}
 
 	@Override
