4d61b57066c72970b876ae3157fa626ab9ed6f1f
SleekPanther <macrobyte2@gmail.com>
2017-11-28 07:42:57 -0500

    python extract to functions, better testing

--- n-choose-k.py
+++ n-choose-k.py
@@ -1,22 +1,35 @@
-tiles=['a','b','c','d','e','f']
-count=0
-combinations=[]
-
 #Implement n choose 3 to find all possible subsets of size 3
-for i in range(len(tiles)):
-    for j in range((i+1),len(tiles)):
-        for k in range((j+1), len(tiles)):
-            print(tiles[i], tiles[j], tiles[k])
-            combinations.append([tiles[i], tiles[j], tiles[k]])
-            count+=1
-print('\nCount =',count)
+def printCombinations(cards):
+	count=0
+	combinations=[]
 
+	for i in range(len(cards)):
+		for j in range((i+1),len(cards)):
+			for k in range((j+1), len(cards)):
+				print(cards[i], cards[j], cards[k])
+				combinations.append([cards[i], cards[j], cards[k]])
+				count+=1
+	print('Combination Count =',count)
+	return combinations
+
+def main():
+	cards=['a','b','c','d','e','f']
+
+	combinations=printCombinations(cards)
+
+	print("\nAre Combinations unique?", areCombinationsDistinct(combinations))
+	
+	combinations.append(['a', 'b', 'c'])
+	print("\nAdded duplicate combination ['a', 'b', 'c'] to test uniqueness")
+	print("Are Combinations unique?", areCombinationsDistinct(combinations))
+
+#Search for duplicates to verify n choose 3 algorithm works
 def areCombinationsDistinct(combinations):
-    for i in range(len(combinations)):
-        for j in range((i+1), len(combinations)):
-            if (combinations[i][0] in combinations[j]) and (combinations[i][1] in combinations[j]) and (combinations[i][2] in combinations[j]):
-                return False
-    return True
+	for i in range(len(combinations)):
+		for j in range((i+1), len(combinations)):
+			#Check if another combination contains all 3 cards that the current combination contains
+			if (combinations[i][0] in combinations[j]) and (combinations[i][1] in combinations[j]) and (combinations[i][2] in combinations[j]):
+				return False
+	return True
 
-#combinations.append(['c', 'd', 'e'])
-print(areCombinationsDistinct(combinations))
+main()
